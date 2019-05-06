3e4e039949af35cb921c4f7822e6508d03789c24
Erki Luik <erki.luik.001@khk.ee>
2019-05-06 09:59:43 +0300

    yl3.sh

---
+++ praks4a/yl3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#backup koostaja
+tar -czf /home/user/skriptimine/praks4a/backup/test.tar.gz /home/user/skriptimine/praks4a/test #võtab test kasuta ja pakkib ta tar.zip failiks ning paneb selle backup kasuta
+echo kataloogi /home/user/skriptimine/praks4a/test/ backup-i nimi on test.backup.tar.gz ja ta asub /home/user/skriptimine/prask4a/backup/ kataloogis #seletab ära mida praegu tehti
+#
+#lõpp
