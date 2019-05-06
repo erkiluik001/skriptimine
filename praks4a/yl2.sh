d0c01eb311eeadf22a31e555b9183ee07aaf4c6f
Erki Luik <erki.luik.001@khk.ee>
2019-05-06 09:43:25 +0300

    yl2.sh

--- praks4a/yl2.sh
+++ praks4a/yl2.sh
@@ -2,11 +2,11 @@
 #!/bin/bash
 #
 #ringi arvutaja
-echo -n "Sisesta oma ringi raadius (cm):"
-read cm
-S=`echo "3.14 * ( $cm * $cm )" | bc`
-U=`echo "2 * 3.14 * $cm" | bc`
-echo "Sinu ringi pindala on $S ruutsentimeetrit ja ümbermõõt on $U cm."
+echo -n "Sisesta oma ringi raadius (cm):" #kasutaja peab sisestama raadiuse mõõdu
+read cm #loeb kasutaja vastust
+S=`echo "3.14 * ( $cm * $cm )" | bc` #valem pindala arvutamiseks
+U=`echo "2 * 3.14 * $cm" | bc` #valem ümbermõõdu arvutamiseks
+echo "Sinu ringi pindala on $S ruutsentimeetrit ja ümbermõõt on $U cm." #annab vastused
 #
 #lõpp
 
