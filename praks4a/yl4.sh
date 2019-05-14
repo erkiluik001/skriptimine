2990b29b50259fd5a4a38225d6c143aa02d4a699
Erki Luik <erki.luik.001@khk.ee>
2019-05-14 15:53:16 +0300

    yl4.sh

--- praks4a/yl4.sh
+++ praks4a/yl4.sh
@@ -3,7 +3,7 @@
 #kataloogi pakkimine
 echo -n "Sisesta kataloogi nimi mida tahad pakkid:" #küsib kasutajalt kataloogi nime mida soovib pakkida
 read kata #loeb kasutaja vastust
-kt=`echo $kata | cut -c 15-30`
+kt=$(echo $kata | awk -F/ '{print $NF}')
 tar -czf $kt.tar.gz $kata #teeb kasutaja soovitud kataloogi pakkiud kataloogiks
 mv $kt.tar.gz /home/user/skriptimine/praks4a/backup #liigutab just pakkitud kataloogi backup kataloogi mille kasutaja valis
 echo "kataloogi $kata backup-i nimi on $kt.tar.gz ja ta asub /home/user/skriptimine/praks4a/backup" #seletab kasutajale mis juhtus
